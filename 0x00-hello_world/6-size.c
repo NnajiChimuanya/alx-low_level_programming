875ac1e14e85b40800982f13a0200ffc147953a6
Nnaji Chimuanya <nnajichimuanya@gmail.com>
2022-12-11 07:13:32 -0800

    m

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+	long int b;
+	long long int c;
+	char d;
+	float e;
+
+	printf("size of a char: %lu byte(s)\n", (unsigned long)sizeof(d)
+}
