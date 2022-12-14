1a519a34527abfd539e4f2b26528fc5625b37eb7
Nnaji Chimuanya <nnajichimuanya@gmail.com>
2022-12-13 20:11:43 -0800

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - Comutes the ablsolute value of an interger
+ * @n: The interger to be computed
+ *
+ * Return: The absloute value of the interger.
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
