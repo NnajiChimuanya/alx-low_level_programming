77d62ed7bc417635fce9808e1bbe9d6e0de68667
Nnaji Chimuanya <nnajichimuanya@gmail.com>
2022-12-13 20:38:20 -0800

    hj

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -4,7 +4,7 @@
  * in order separated by a coma followed by a space
  * @n: The number to begin counting at
  */
-void print_to_98(int n) 
+void print_to_98(int n)
 {
 	if (n >= 98)
 	{
@@ -12,7 +12,7 @@ void print_to_98(int n)
 			printf("%d, ", n--);
 		printf("%d\n", n);
 	}
-	else 
+	else
 	{
 		while (n < 98)
 			printf("%d, ", n++);
