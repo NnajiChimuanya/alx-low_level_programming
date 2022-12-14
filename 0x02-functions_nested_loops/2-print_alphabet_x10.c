9b9327ef4e953ca73f3423ce0fb841c4ee5aee20
Nnaji Chimuanya <nnajichimuanya@gmail.com>
2022-12-13 20:54:04 -0800

    correct input

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - Prints 10 times the alphabets, in lowercase, followed by a new line"
+ * print_alphabet_x10 - Prints 10 times the alphabets, 
+ * in lowercase, followed by a new line"
  *
  * Return: Always 0.
  */
@@ -10,7 +11,7 @@ void print_alphabet_x10(void)
 	int count = 0;
 	char letter;
 
-	while (0 < 10)
+	while (count < 10)
 	{
 		for (letter = 'a'; letter <= 'z'; letter++)
 			_putchar(letter);
